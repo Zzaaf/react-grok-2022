7e47b70dd15bf9f7376a6632846e991df39eb270
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-04-13 17:43:58 +0300

    Added test 120

--- client/src/components/answers/Puzzle120/Puzzle120.jsx
+++ client/src/components/answers/Puzzle120/Puzzle120.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from './Card';
 
-function Puzzle120(props) {
+// Установи npm-пакет bootstrap (не путать с CDN), и воспользуйся уже готовыми решениями этой css-библиотеки.
+// В качестве готового решения нужно найти "Card" и воспользоваться bootstrap вёрсткой, на основе которой создать компонент <Card>
+// В компонент <Card> нужно пробросить данные из компонента <Puzzle120> через props, чтобы в карточке можно было использовать: "Name", "Date of Birth" и кнопку "Hide card"
+// При нажатии кнопки "Hide card", карточка удаляется из DOM-дерева.
+// Так же компонент <Puzzle120> имеет bootstrap css-класс 'container'
+
+function Puzzle120() {
+
+  const [visible, setVisible] = useState(true)
+
+  const info = {
+    name: 'Linus Torvalds',
+    dateBirth: '28.12.1969'
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {visible && <Card info={info} setVisible={setVisible} />}
+    </>
   );
 }
 
