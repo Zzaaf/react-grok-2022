ccb978c6f913d6bbeebda1d9c7928c7fc6e7958a
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-04-20 17:57:11 +0300

    Added tasks basic03-05 and tests to them

--- client/src/components/answers/Basic03_ElbrusColor/Basic03_ElbrusColor.jsx
+++ client/src/components/answers/Basic03_ElbrusColor/Basic03_ElbrusColor.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Basic03_ElbrusColor.css'
 
 function Basic03_ElbrusColor() {
   return (
-    <div style={{ backgroundColor: '#4520ab', color: '#ffbc5b' }}>
-      <h1>It always seems impossible until it's done</h1>
+    <div className='wrap' style={{ backgroundColor: '#4520ab', color: '#ffbc5b' }}>
+      <h1 className='h1'>It always seems impossible until it's done</h1>
     </div>
   );
 }
