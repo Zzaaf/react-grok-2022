97285812d0fac34a795e801da32cda268e9ed46b
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-07-08 16:29:33 +0300

    Fixes based on issue #7, minor .md file formatting fixes

--- check-env.js
+++ check-env.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
-const getMessage = (dir) =>
-  chalk.bold.red(`Создай файл "${dir}/.env" на основе "${dir}/.env.example"\n`);
+const getMessage = (dir) => {
+  return chalk.bold.red(`Создай файл "${dir}/.env" на основе "${dir}/.env.example"\n`);
+}
 
-function check(dir) {
+const check = (dir) => {
   const envFile = path.join(dir, '.env');
+
   if (!fs.existsSync(envFile)) {
     console.log(getMessage(dir));
     process.exit(1);
